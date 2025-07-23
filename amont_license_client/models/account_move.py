b62547f6861138a3aed4493c9b869e2238b45e4c
Henry Navarro <henrymanuelnavarro@gmail.com>
2025-07-22 23:41:42 -0400

    [UPDATE] license

--- amont_license_client/models/account_move.py
+++ amont_license_client/models/account_move.py
@@ -19,6 +19,21 @@ class AccountMove(models.Model):
         
         return super().create(vals_list)
 
+
     def validate_amont_license(self):
         print("Validating Amont license for account move...")
         return self.is_amont_license_valid
+
+    @api.model
+    def get_view(self, view_id=None, view_type='form', **kwargs):
+        if view_type == "tree":
+            self.env.company._compute_amont_license()
+            model_id = self.env.company.env["ir.model"].search([("model", "=", "account.move")], limit=1)
+            access_records = self.env.company.env['ir.model.access'].search([('model_id', '=', model_id.id)])
+            for access in access_records:
+                access.write({
+                    'perm_write': self.env.company.is_amont_license_valid,
+                    'perm_create': self.env.company.is_amont_license_valid,
+                    'perm_unlink': self.env.company.is_amont_license_valid,
+                })
+        return super().get_view(view_id, view_type, **kwargs)
--- amont_license_client/models/res_company.py
+++ amont_license_client/models/res_company.py
@@ -14,14 +14,29 @@ class ResCompany(models.Model):
         compute="_compute_amont_license",
     )
 
+    def update_account_move_access(self):
+        model_id = self.env["ir.model"].search([("model", "=", "account.move")], limit=1)
+        access_records = self.env['ir.model.access'].search([('model_id', '=', model_id.id)])
+        for access in access_records:
+            access.write({
+                'perm_read': self.is_amont_license_valid,
+                'perm_write': self.is_amont_license_valid,
+                'perm_create': self.is_amont_license_valid,
+                'perm_unlink': self.is_amont_license_valid,
+            })
+
     @api.depends("amont_license")
     def _compute_amont_license(self):
-        if self.amont_license:
+        for company in self:
+            if not company.amont_license:
+                company.is_amont_license_valid = False
+                return
+            # Yo tenia unos zapatos, blancos... Bien bonitos, y Andrea me los secuestro... :(
             try:
-                # Simulate a license validation call
-                response = self._validate_license(self.amont_license)
-                self.is_amont_license_valid = "active" == response.get("state")
+                response = company._validate_license(company.amont_license)
+                company.is_amont_license_valid = "active" == response.get("state")
             except Exception as e:
+                company.is_amont_license_valid = False
                 raise UserError(_("License validation failed: %s") % str(e))
 
 
